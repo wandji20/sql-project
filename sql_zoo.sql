50e4897c0be82e4c2d2497858e8b48efbc6caf15
wandji20 <wbertrand10@gmail.com>
2020-12-28 11:25:49 +0100

    answer to exercise 1

--- sql_zoo.sql
+++ sql_zoo.sql
@@ -33,32 +33,40 @@ SELECT name, area, population
 SELECT name, population/area
   FROM world
  WHERE name IN ('China', 'Nigeria', 'France', 'Australia')
-/*   */
-
-/*   */
-
-/*   */
-
-/*   */
-
-/*   */
-
-/*   */
-
-/*   */
-
-/*   */
-
-/*   */
-
-/*   */
-
-/*   */
-
-/*   */
-
-/*   */
-
+/*Exercise 1   1   */
+SELECT name, continent, population FROM world;
+/* 2  */
+SELECT name
+  FROM world
+ WHERE population > 200000000;
+/* 3  */
+SELECT name, gdp/population FROM world WHERE population > 200000000;
+/* 4  */
+SELECT name, population/1000000 FROM world WHERE continent = 'South America';
+/* 5  */
+SELECT name, population FROM world WHERE name IN ('France', 'Germany', 'Italy');
+/* 6  */
+SELECT name FROM world WHERE name LIKE '%United' OR name LIKE 'United%' OR name LIKE '%United%';
+/* 7  */
+SELECT name, population, area FROM world WHERE area > 3000000 OR population > 250000000;
+/* 8  */
+SELECT name, population, area FROM world WHERE area > 3000000 XOR population > 250000000;
+/* 9  */
+SELECT name, ROUND(population/1000000, 2), ROUND(gdp/1000000000, 2) FROM world WHERE continent = 'South America';
+/* 10 */
+SELECT name , ROUND(ROUND(gdp/population, 0)/1000, 0)*1000 FROM world WHERE gdp/1000000000000 > 1;
+/* 11  */
+SELECT name, capital FROM world WHERE LENGTH(capital) = LENGTH(name);
+/* 12  */
+SELECT name, capital FROM world WHERE LEFT(name, 1) = LEFT(capital, 1) AND name != capital;
+/* 13  */
+SELECT name FROM world
+    WHERE name LIKE '%a%'
+    AND name NOT LIKE '% %'
+    AND name LIKE '%e%'
+    AND name LIKE '%i%'
+    AND name LIKE '%o%'
+    AND name LIKE '%u%';
 /*   */
 
 /*   */
