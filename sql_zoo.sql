b3c0705d33a88131fc8637d2c18876c4c9b47abf
wandji20 <wbertrand10@gmail.com>
2020-12-29 22:26:00 +0100

    complete answers for exercise 6

--- sql_zoo.sql
+++ sql_zoo.sql
@@ -270,21 +270,35 @@ SELECT player, teamid, coach, gtime
   FROM goal JOIN eteam on teamid=id
  WHERE gtime<=10
 /* 6  */
-
+SELECT mdate, teamname FROM game JOIN eteam ON (team1=eteam.id) WHERE coach = 'Fernando Santos'
 /* 7  */
-
+SELECT player FROM game JOIN goal ON (matchid=id) WHERE stadium = 'National Stadium, Warsaw';
 /* 8  */
-
-/*   */
-
-/*   */
-
-/*   */
-
-/*   */
-
-/*   */
-
+SELECT DISTINCT player
+  FROM game JOIN goal ON matchid = id 
+    WHERE (team1='GER' OR team2='GER') AND teamid <> 'GER' 
+/* 9  */
+SELECT teamname, COUNT(player)
+  FROM eteam JOIN goal ON id=teamid GROUP BY teamname
+/* 10  */
+SELECT stadium, COUNT(player)
+FROM game JOIN goal ON matchid = id GROUP BY stadium
+/* 11  */
+SELECT matchid, mdate, COUNT(player)
+  FROM game JOIN goal ON matchid = id 
+ WHERE (team1 = 'POL' OR team2 = 'POL') GROUP BY matchid, mdate
+/*  12 */
+SELECT matchid, mdate, COUNT(player)
+  FROM game JOIN goal ON matchid = id 
+ WHERE (team1 = 'GER' OR team2 = 'GER') AND teamid = 'GER' GROUP BY matchid, mdate
+/* 13  */
+SELECT mdate,
+  team1,
+  SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) AS score1,
+team2,
+SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) AS score2
+  FROM game LEFT JOIN goal ON matchid = id
+GROUP BY mdate, team1, team2 ORDER BY mdate,matchid,team1,team2
 /*   */
 
 /*   */
