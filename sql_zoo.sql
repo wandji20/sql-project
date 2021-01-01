f8ec2c3b60f0d0c341c3fdd4c531abf0fdd7b2e1
wandji20 <wbertrand10@gmail.com>
2021-01-01 02:25:39 +0100

    add queries for self join

--- sql_zoo.sql
+++ sql_zoo.sql
@@ -511,20 +511,28 @@ Splint	Computing
 Spiregrain	Other
 Cutflower	Other
 Deadyawn	Other
-
-
 /*  SELF JOIN 1  */
-
+SELECT COUNT(* )FROM stops
 /* 2  */
-
+SELECT id FROM stops WHERE name='Craiglockhart'
 /* 3  */
-
+SELECT id, name FROM stops JOIN route ON id=stop WHERE company='LRT' AND num='4'
 /* 4  */
-
+SELECT company, num, COUNT(*)
+FROM route WHERE stop=149 OR stop=53
+GROUP BY company, num HAVING COUNT(*)=2
 /* 5  */
-
+SELECT a.company, a.num, a.stop, b.stop
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+WHERE a.stop=53 AND b.stop=(SELECT id FROM stops WHERE name='London Road')
 /* 6  */
-
+SELECT a.company, a.num, stopa.name, stopb.name
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' AND stopb.name='London Road'
 /* 7  */
 SELECT DISTINCT a.company, a.num FROM route a JOIN route b ON a.num=b.num WHERE a.stop=115 AND b.stop = 137 
 /*  8 */
