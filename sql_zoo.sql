b22a83d4363b07818764d28ec89117bad5b7a2af
wandji20 <wbertrand10@gmail.com>
2021-01-01 02:11:01 +0100

    Add using null quiz

--- sql_zoo.sql
+++ sql_zoo.sql
@@ -454,7 +454,7 @@ Table-B
 A Bronx Tale	1993
 Bang the Drum Slowly	1973
 Limitless	2011
-/* Using NULL 1  */
+/* Using 1  */
 SELECT name FROM teacher WHERE dept IS NULL
 /*  2 */
 SELECT teacher.name, dept.name
@@ -493,3 +493,22 @@ SELECT name,CASE
 ELSE 'None'
 END
   FROM teacher
+/* Quiz 1  */
+ SELECT teacher.name, dept.name FROM teacher LEFT OUTER JOIN dept ON (teacher.dept = dept.id)
+/*  2 */
+ SELECT dept.name FROM teacher JOIN dept ON (dept.id = teacher.dept) WHERE teacher.name = 'Cutflower'
+/* 3  */
+ SELECT dept.name, COUNT(teacher.name) FROM teacher RIGHT JOIN dept ON dept.id = teacher.dept GROUP BY dept.name
+/* 4  */
+display 0 in result column for all teachers without department
+/* 5  */
+'four' for Throd
+/* 6  */
+Table-A
+Shrivell	Computing
+Throd	Computing
+Splint	Computing
+Spiregrain	Other
+Cutflower	Other
+Deadyawn	Other
+
