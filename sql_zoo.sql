4e095de220072396da9d26ddefa9739e4eef4a7e
wandji20 <wbertrand10@gmail.com>
2021-01-01 02:02:36 +0100

    Add query for Using NULL

--- sql_zoo.sql
+++ sql_zoo.sql
@@ -454,4 +454,42 @@ Table-B
 A Bronx Tale	1993
 Bang the Drum Slowly	1973
 Limitless	2011
-
+/* Using NULL 1  */
+SELECT name FROM teacher WHERE dept IS NULL
+/*  2 */
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id)
+/* 3  */
+SELECT teacher.name, dept.name
+ FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id)
+/* 4  */
+SELECT teacher.name, dept.name
+ FROM teacher RIGHT JOIN dept
+           ON (teacher.dept=dept.id)
+/* 5  */
+SELECT name,COALESCE(mobile,'07986 444 2266') FROM teacher 
+/* 6  */
+SELECT teacher.name, COALESCE(dept.name,'None')
+ FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id)
+/*  7 */
+SELECT COUNT(name), COUNT(mobile) FROM teacher
+/*  8 */
+SELECT dept.name, COUNT(teacher.name) FROM dept LEFT JOIN teacher ON (teacher.dept=dept.id) GROUP BY dept.name
+/* 9  */
+SELECT name,CASE
+  WHEN dept= 1 THEN 'Sci'
+  WHEN dept= 2 THEN 'Sci'
+ELSE 'Art'
+END
+  FROM teacher
+/* 10  */
+SELECT name,CASE
+  WHEN dept= 1 THEN 'Sci'
+  WHEN dept= 2 THEN 'Sci'
+  WHEN dept= 3 THEN 'Art'
+ELSE 'None'
+END
+  FROM teacher
