428b00ff00f9a115bd7169832dbd91343c2f66a1
wandji20 <wbertrand10@gmail.com>
2021-01-01 01:59:44 +0100

    Add query for more JOIN quiz

--- sql_zoo.sql
+++ sql_zoo.sql
@@ -422,3 +422,36 @@ SELECT title, COUNT(actorid) FROM casting
   ORDER BY COUNT(actorid) DESC, title
 /* 15  */
 SELECT name FROM actor JOIN casting ON actorid=id WHERE movieid IN (  SELECT movieid FROM casting JOIN actor ON id=actorid   WHERE  actorid = (SELECT id FROM actor WHERE name='Art Garfunkel')) AND name !='Art Garfunkel'
+/* Quiz 1  */
+SELECT name
+  FROM actor INNER JOIN movie ON actor.id = director
+ WHERE gross < budget
+/* 2  */
+SELECT *
+  FROM actor JOIN casting ON actor.id = actorid
+  JOIN movie ON movie.id = movieid
+/*  3 */
+SELECT name, COUNT(movieid)
+  FROM casting JOIN actor ON actorid=actor.id
+ WHERE name LIKE 'John %'
+ GROUP BY name ORDER BY 2 DESC
+/*  4 */
+Table-B
+"Crocodile" Dundee
+Crocodile Dundee in Los Angeles
+Flipper
+Lightning Jack
+/* 5  */
+SELECT name
+  FROM movie JOIN casting ON movie.id = movieid
+  JOIN actor ON actor.id = actorid
+WHERE ord = 1 AND director = 351
+/* 6  */
+link the director column in movies with the primary key in actor
+connect the primary keys of movie and actor via the casting table
+/*  7 */
+Table-B
+A Bronx Tale	1993
+Bang the Drum Slowly	1973
+Limitless	2011
+
