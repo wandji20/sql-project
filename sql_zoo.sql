df61d52c86bfa2e7604de56c743e0e2a20a4b530
wandji20 <wbertrand10@gmail.com>
2021-01-01 14:33:04 +0100

    Add queries for nss

--- sql_zoo.sql
+++ sql_zoo.sql
@@ -550,7 +550,7 @@ SELECT stopb.name, a.company, a.num
                WHERE stopa.name= 'Craiglockhart'
 /* 10  */
 
-/*   */
+/* Quiz 1  */
 
 /*   */
 
@@ -558,22 +558,55 @@ SELECT stopb.name, a.company, a.num
 
 /*   */
 
-/*   */
-
-/*   */
-
-/*   */
-
-/*   */
-
-/*   */
-
-/*   */
-
-/*   */
-
-/*   */
-
+/* nss query  1  */
+SELECT A_STRONGLY_AGREE
+  FROM nss
+ WHERE question='Q01'
+   AND institution='Edinburgh Napier University'
+   AND subject='(8) Computer Science'
+/*  2 */
+SELECT institution, subject
+  FROM nss
+ WHERE question='Q15'
+   AND score>=100
+/* 3  */
+SELECT institution,score
+  FROM nss
+ WHERE question='Q15'
+   AND score<50
+   AND subject='(8) Computer Science'
+/* 4  */
+SELECT subject, SUM(response)
+  FROM nss
+ WHERE question='Q22'
+   AND subject IN ('(8) Computer Science', '(H) Creative Arts and Design')
+GROUP BY subject
+/* 5  */
+SELECT subject, SUM((A_STRONGLY_AGREE/100)*response)
+  FROM nss
+ WHERE question='Q22'
+   AND (subject='(H) Creative Arts and Design'
+   OR subject='(8) Computer Science')
+GROUP BY subject
+/* 6  */
+SELECT subject, ROUND(SUM((A_STRONGLY_AGREE)*response)/SUM(response))
+  FROM nss
+ WHERE question='Q22'
+   AND (subject='(H) Creative Arts and Design'
+   OR subject='(8) Computer Science')
+GROUP BY subject
+/* 7  */
+SELECT institution, ROUND(AVG(score*response/100)*100/AVG(response))
+  FROM nss
+ WHERE question='Q22'
+   AND (institution LIKE '%Manchester%')
+GROUP BY institution
+/* 8  */
+SELECT institution, SUM(sample), SUM(case when subject='(8) Computer Science' then sample end)
+FROM nss
+WHERE institution LIKE '%Manchester%'
+   AND question='Q01' 
+GROUP BY institution
 /*   */
 
 /*   */
