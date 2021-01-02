63277649a00d95f45e10bd6ea0a865ff847ebbd9
wandji20 <wbertrand10@gmail.com>
2021-01-02 02:26:07 +0100

    Start query for window function

--- sql_zoo.sql
+++ sql_zoo.sql
@@ -607,14 +607,31 @@ FROM nss
 WHERE institution LIKE '%Manchester%'
    AND question='Q01' 
 GROUP BY institution
-/*   */
+/* Windows Functions   1   */
+SELECT lastName, party, votes
+  FROM ge
+ WHERE constituency = 'S14000024' AND yr = 2017
+ORDER BY votes DESC
+/*  2 */
+SELECT party, votes,
+       RANK() OVER (ORDER BY votes DESC) as posn
+  FROM ge
+ WHERE constituency = 'S14000024' AND yr = 2017
+ORDER BY party
+/* 3  */
+SELECT yr,party, votes,
+      RANK() OVER (PARTITION BY yr ORDER BY votes DESC) as posn
+  FROM ge
+ WHERE constituency = 'S14000021'
+ORDER BY party,yr
+/* 4  */
+SELECT constituency,party, votes, RANK() OVER (PARTITION BY constituency ORDER BY votes DESC ) AS posn
+  FROM ge
+ WHERE constituency BETWEEN 'S14000021' AND 'S14000026'
+   AND yr  = 2017
+ORDER BY posn, constituency
 
-/*   */
 
-/*   */
+/* 5  */
 
-/*   */
-
-/*   */
-
-/*   */
+/* 6  */
